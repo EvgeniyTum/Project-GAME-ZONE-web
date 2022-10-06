5403dbe04754e320dc5cebf80c5717e08faa745d
EvgenyTum <gotic@list.ru>
2022-10-06 23:08:55 +0300

    change chess.js & style.css

--- js/chess.js
+++ js/chess.js
@@ -17,9 +17,11 @@ export const chessTableOfPlayers = (obj) => {
       <br>
       <input type="text" class="input__players" placeholder="Имена через запятую">
     </div>
-    <div>
-      <input type="radio" name="game"> Турнир
-      <input type="radio" name="game"> Обычная игра
+    <div class="checkbox__type">
+      <input class="type__game" type="radio" name="game">
+      <div>Турнир</div>
+      <input class="type__game" type="radio" name="game">
+      <div>Обычная игра</div>
     </div>
   </form>
   <div class="btn">Старт!</div>
@@ -30,17 +32,94 @@ export const chessTableOfPlayers = (obj) => {
     even.preventDefault();
 
     const playersList = document.querySelector('.input__players');
-    playersList.value.split(',').forEach((item, i) => {
-      item.trim();
-      const player = {player: item, score: 0};
+    const typeGame = document.querySelector('.type__game');
+
+    playersList.value.split(',').forEach((item) => {
+      const player = {player: item.trim(), score: 0};
       chessPlayers.players.push(player);
     });
-    
-    console.log(chessPlayers);
 
-    playingField.innerHTML = `<div>Мы работаем над этим =)</div>`;
+    if (typeGame.checked) {
+      chessPlayers.typeGame.push('Турнир');
+    } else {
+      chessPlayers.typeGame.push("Обычная игра");
+    }
+
+    // console.log(chessPlayers);
+
+    // playingField.innerHTML = `<div>Мы работаем над этим =)</div>`;
+
+    playingField.innerHTML = '';
+
+    let table = document.createElement('table');
+    table.classList.add('chess__table');
+    playingField.appendChild(table);
+
+    for (let i = 0; i < chessPlayers.players.length + 1; i++) {
+      let tr = document.createElement('tr');
+      table.appendChild(tr);
+      for (let j = 0; j <chessPlayers.players.length + 1; j++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        if (i === 0 & j > 0) {
+          td.innerHTML = `${chessPlayers.players[j - 1].player}`;
+        } if (i === 0 & j === 0) {
+          td.innerHTML = 'Имя игрока';
+        } if (i > 0 && j === 0) {
+          td.innerHTML = `${chessPlayers.players[i - 1].player}`;
+        } if (i === j && i > 0) {
+          td.style.cssText = `background: grey`;
+        } else if (i !== j && i > 0 && j > 0) {
+          td.innerHTML = `
+          <input type="number" min="0" max="1" step="0.5">
+          <button class="ok">ok</button>
+          `;
+
+        }
+
+        const valueTableChess = document.querySelectorAll('td');
+
+        valueTableChess.forEach((item) => {
+        
+        });
+      }
+    }
+
+    // let valueTableChessArr = [];
+    // for (let i = 1; i <= chessPlayers.players.length; i++) {
+    //   let arr =[];
+    //   arr.length = chessPlayers.players.length;
+    //   valueTableChessArr.push(arr);
+    //   arr = arr.slice(arr.length - 1);
+    // }
+
+    
+    let chessTableInfo = document.createElement('div');
+    playingField.appendChild(chessTableInfo);
+    chessTableInfo.style.cssText = `
+      text-align: left;
+      margin-top: 10px;
+      font-size: 10px;
+    `;
+    chessTableInfo.innerHTML = `
+      <div>Строка - кто играет!</div>
+      <div>Столбец - с кем играет!</div>
+      <div>Победа - поставь 1</div>
+      <div>Поражение - поставь 0</div>
+      <div>Ничья - поставь 0,5</div>
+    `;
+
+    let endGameChess = document.createElement('div');
+    endGameChess.classList.add('btn');
+    endGameChess.innerHTML = 'Закончить игру!';
+    playingField.appendChild(endGameChess);
+
+
+    // console.log(valueTableChessArr);
+    console.log(table);
+
+
   });
 };
-
 
 
